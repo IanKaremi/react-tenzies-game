78292a2717d1fd89ae7c2686e75f8821750d294f
IanKaremi <karemiianofficial@gmail.com>
2025-05-18 18:17:04 +0300

    added reroll function and flavor text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState } from 'react'
 import './App.css'
 import Die from './die'
 
@@ -11,33 +11,65 @@ function App() {
   
   function getDiceNumbers() {
     var diceArray = []
-    diceArray.length = 10
+
+   
     for (let i = 0; i < 10; i++) {
       //generate a random number between 1 and 6
-      //and push it to each object then to the diceArray
-      var obj = {isHeld: false, value: Math.floor(Math.random() * 6) + 1}
-      diceArray.push(obj)
+          var obj = {isHeld: false, value: Math.floor(Math.random() * 6) + 1}
+          diceArray.push(obj)
+     
+     
     } 
-    console.log("diceArray: " + diceArray)
+    //console.log("diceArray: " + diceArray)
     return diceArray
   }
+
+  
+
+  function holdDie(key){
+    setDice(prevDice => {
+      return prevDice.map((die, index) => {
+        //if the index of the die is equal to the key passed in, set isHeld to true
+        if (index === key) {
+          return {...die, isHeld: !die.isHeld}
+        } else {
+          return die
+        }
+      })
+    })
+    console.log("index: " + key)
+  };
+
+  function rollDice() {
+    setDice(prevDice => prevDice.map(die=>
+      die.isHeld ? die :  {...die, value: Math.floor(Math.random() * 6) + 1}
+        //if the die is held, return it as is
+      
+    ))
+    
+  }
+
+
 
   //map through the diceArray and create a die element for each number
   var dieElements = dice.map((die, index) => {
     return (
-      <Die key={index} value={die.value} held={die.isHeld}/>
+      <Die key={index} id={index}  value={die.value} held={die.isHeld} hold={holdDie}/>
     )
   }
   )
 
-  console.log(dice)
+
+  //console.log(dice)
   return (
     <>
      <main className='mainEl'>
+        <h1 className='title'>Tenzies</h1>
+        <p className='instructions'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className='diceGrid'>
         {dieElements}
         </div>
-        <button className='rollBtn' onClick={() => setDice(getDiceNumbers())}>Roll</button>
+        <button className='rollBtn' onClick={rollDice}>Roll</button>
 
 
      </main>
--- src/die.jsx
+++ src/die.jsx
@@ -3,6 +3,7 @@ export default function Die(props) {
         <button 
             //conditional styling for button depending on if it is held or not
             style = {{backgroundColor: props.held ? "#59E391" : "white"}}
+            onClick={()=>props.hold(props.id)}
         >
             {props.value}
         </button>
